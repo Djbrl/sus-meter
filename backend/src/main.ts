4a95a9727288de4597c7d407cf0a013e1ce96af1
Djibril Sy <dsy@MacBook-Pro-de-Djibril.local>
2023-12-24 11:15:58 +0100

    minor update to main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cors from 'cors';
+  import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
+  import * as cors from 'cors';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-        `${process.env.FRONTEND_IP}:${process.env.FRONTEND_HOST_PORT}`,
-    ],
-    credentials: true,
-  });
-  await app.listen(process.env.BACKEND_PORT_VAR || 3000);
-}
-bootstrap();
+    app.enableCors({
+      origin: [
+          '*'
+          // `${process.env.FRONTEND_IP}:${process.env.FRONTEND_HOST_PORT}`,
+      ],
+      credentials: true,
+    });
+    await app.listen(process.env.BACKEND_PORT_VAR || 3000);
+  }
+  bootstrap();
