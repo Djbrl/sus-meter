b44a9cc445ee1e185ecd3f112247d5d3c18bf71e
Djibril Sy <dsy@MacBook-Pro-de-Djibril.local>
2023-12-24 12:39:26 +0100

    expanded cors backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,13 +14,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, { httpsOptions });
 
-  app.enableCors({
-    origin: [
-        '*'
-        // `${process.env.FRONTEND_IP}:${process.env.FRONTEND_HOST_PORT}`,
-    ],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(process.env.BACKEND_PORT_VAR || 3000);
 }
 bootstrap();
