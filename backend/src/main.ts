9861fc6d52a15911d101dfd7bcfa4cbf4469f289
Djibril Sy <dsy@MacBook-Pro-de-Djibril.local>
2023-12-24 11:39:58 +0100

    added express and fs to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,26 +1,26 @@
-  import { NestFactory } from '@nestjs/core';
-  import { AppModule } from './app.module';
-  import * as cors from 'cors';
-  import * as fs from 'fs';
-  import * as https from 'https';
-  
-  async function bootstrap() {
- 
-	   // Load SSL certificate and private key
-  const serverOptions = {
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cors from 'cors';
+import * as fs from 'fs';
+import * as https from 'https';
+
+async function bootstrap() {
+
+    // Load SSL certificate and private key
+  const httpsOptions = {
     key: fs.readFileSync('/etc/letsencrypt/live/sus-meter.com/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/sus-meter.com/fullchain.pem'),
   };
 
-    const app = await NestFactory.create(AppModule, {serverOptions});
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
-    app.enableCors({
-      origin: [
-          '*'
-          // `${process.env.FRONTEND_IP}:${process.env.FRONTEND_HOST_PORT}`,
-      ],
-      credentials: true,
-    });
-    await app.listen(process.env.BACKEND_PORT_VAR || 3000);
-  }
-  bootstrap();
+  app.enableCors({
+    origin: [
+        '*'
+        // `${process.env.FRONTEND_IP}:${process.env.FRONTEND_HOST_PORT}`,
+    ],
+    credentials: true,
+  });
+  await app.listen(process.env.BACKEND_PORT_VAR || 3000);
+}
+bootstrap();
